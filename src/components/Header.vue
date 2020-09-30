2ed5ceb859c6a2cb497b7b86404530055e21b853
EJ <deathlybeat@icloud.com>
2020-09-29 20:43:10 -0500

    Cleaning up code from cli.

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header">
+        <h1>Weather Vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+.header {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+h1 {
+    margin: 28px;
+}
+</style>
